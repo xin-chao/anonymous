82c72042dc60e99727baf2e4f3479763337246d1
xin-chao <ojiattacker@gmail.com>
2026-08-28 18:24:53 +0700

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -97,6 +97,9 @@ MODELS = [
     "gemini-3.7-flash",
     "gemini-3.6-flash",
     "gemini-3.5-flash",
+    "gemini-3-flash-preview",
+    "gemini-3.5-flash-lite",
+    "gemini-3.1-flash-lite",
 ]
 
 
